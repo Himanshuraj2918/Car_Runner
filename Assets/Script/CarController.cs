5c7dc5761b1bab3bdd6d6a8a7505d0fa91e2c035
Himanshu2918 <himanshuraj2918@gmail.com>
2025-01-22 23:03:27 +0530

    provided rotation to wheel

--- Assets/Script/CarController.cs
+++ Assets/Script/CarController.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public WheelCollider frontRightWheelCollider;
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider backRightWheelCollider;
+    public WheelCollider backLeftWheelCollider;
+
+    public Transform frontRightWheelTransform;
+    public Transform frontLeftWheelTransform;
+    public Transform backRightWheelTransform;
+    public Transform backLeftWheelTransform;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +20,29 @@ public class CarController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MotorForce();
+        UpdateWheel();
+    }
+
+    void MotorForce(){
+        frontRightWheelCollider.motorTorque = 10f;
+        frontLeftWheelCollider.motorTorque = 10f;
+    }
+
+    void UpdateWheel(){
+        RotateWheel(frontRightWheelCollider, frontRightWheelTransform);
+        RotateWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        RotateWheel(backRightWheelCollider, backRightWheelTransform);
+        RotateWheel(backLeftWheelCollider, backLeftWheelTransform);
+    }
+
+    void RotateWheel(WheelCollider wheelCollider, Transform wheelTransform){
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
     }
 }
